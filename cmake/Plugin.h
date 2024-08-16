394d95ac150af588b66f1adb9a13717c32769496
Beef <beef.buns@proton.me>
2024-08-15 17:13:04 -0700

    adds some cmake changes and a KID file

---
+++ cmake/Plugin.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string_view>
+
+namespace Plugin
+{
+	using namespace std::literals;
+
+	inline constexpr REL::Version VERSION
+	{
+		// clang-format off
+		1u,
+		0u,
+		0u,
+		// clang-format on
+	};
+
+	inline constexpr auto NAME = "OBody"sv;
+}
